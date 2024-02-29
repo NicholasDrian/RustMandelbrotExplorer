213075cce0c59261b98b9a5d73e1b2fb83ca5ffb
Nick Drian <nickdrian@Nicks-MacBook-Air.local>
2024-02-28 21:05:05 -0700

    added pkg

---
+++ pkg/mandelbrot_explorer_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function run(): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function wasm_bindgen__convert__closures__invoke1_mut__h34ef76b18c909240(a: number, b: number, c: number): void;
+export function wasm_bindgen__convert__closures__invoke0_mut__hde28a5863ae0508e(a: number, b: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h41922dc30ced710e(a: number, b: number, c: number): void;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_start(): void;
